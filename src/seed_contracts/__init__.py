dc1a8ab04d1a0111af1bdec0ff2cbbde6e62e4e2
zhenrez <think.tattoo.viz@gmail.com>
2026-09-19 03:15:53 -0700

    feat: add seed contract package

---
+++ src/seed_contracts/__init__.py
@@ -0,0 +1,25 @@
+"""Language-neutral semantic contracts for R2D2/K2S0 interoperability."""
+
+from .semantic import (
+    AdmissionDisposition,
+    AdmissionResult,
+    AdmissionStatus,
+    CanonicalEffect,
+    ContractViolation,
+    ReadinessResult,
+    RepresentationSnapshotRef,
+    SemanticBasisRef,
+    SubjectRef,
+)
+
+__all__ = [
+    "AdmissionDisposition",
+    "AdmissionResult",
+    "AdmissionStatus",
+    "CanonicalEffect",
+    "ContractViolation",
+    "ReadinessResult",
+    "RepresentationSnapshotRef",
+    "SemanticBasisRef",
+    "SubjectRef",
+]
